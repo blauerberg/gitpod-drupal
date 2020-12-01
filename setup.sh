fc7136b21b09a176e1b6eb1bb28f5edc4a3bf610
Yoshikazu Aoyama <yskz.aoyama@gmail.com>
2020-12-01 21:06:16 +0900

    oops, installation failed..

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ composer install
 
 vendor/bin/drush -y site:install \
   standard \
-  --site-name="gitpod-drupal"
+  --site-name="gitpod-drupal" \
   --account-name="admin" \
   --account-pass="$(pwgen -c -n -y -s 12 1)" \
   --db-url=sqlite://sites/default/files/.ht.sqlite
