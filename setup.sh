7fa9d7e51a6b54970ffed1e49ef7ece29130d655
Yoshikazu Aoyama <yskz.aoyama@gmail.com>
2020-12-01 20:45:08 +0900

    set site name for installation

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ gp env drupal_passwd="$(pwgen -c -n -y -s 12 1)"
 
 vendor/bin/drush -y site:install \
   standard \
+  --site-name="gitpod-drupal"
   --account-name="admin" \
   --account-pass="$(gp env|grep drupal_passwd|cut -c15-)" \
   --db-url=sqlite://sites/default/files/.ht.sqlite
