3365b9be66483bd0b24941a07de7aad31834dfc6
Yoshikazu Aoyama <yskz.aoyama@gmail.com>
2020-12-01 20:38:04 +0900

    install drupal automatically

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#/bin/sh
+
+composer global require hirak/prestissimo
+composer install
+gp env drupal_passwd="$(pwgen -c -n -y -s 12 1)"
+
+vendor/bin/drush -y site:install \
+  standard \
+  --account-name="admin" \
+  --account-pass="$(gp env|grep drupal_passwd|cut -c15-)" \
+  --db-url=sqlite://sites/default/files/.ht.sqlite
