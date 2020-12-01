8cd3d2be656f3361e2ef63f8d0832484fa47bdf3
Yoshikazu Aoyama <yskz.aoyama@gmail.com>
2020-12-01 21:01:19 +0900

    do not use "gp env"

--- setup.sh
+++ setup.sh
@@ -2,11 +2,10 @@
 
 composer global require hirak/prestissimo
 composer install
-gp env drupal_passwd="$(pwgen -c -n -y -s 12 1)"
 
 vendor/bin/drush -y site:install \
   standard \
   --site-name="gitpod-drupal"
   --account-name="admin" \
-  --account-pass="$(gp env|grep drupal_passwd|cut -c15-)" \
+  --account-pass="$(pwgen -c -n -y -s 12 1)" \
   --db-url=sqlite://sites/default/files/.ht.sqlite
