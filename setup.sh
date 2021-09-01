0179f6703c03b102633fef08a136e94a685d2e3d
blauerberg <yskz.aoyama@gmail.com>
2021-09-01 00:33:01 +0000

    add drupal/coder

--- setup.sh
+++ setup.sh
@@ -11,8 +11,14 @@ composer require drush/drush
 composer require drupal/devel
 composer require drupal/twig_debugger
 composer require drupal/admin_toolbar
+composer require drupal/coder
 composer install
 
+# Setup the phpcs standard
+vendor/bin/phpcs --config-set installed_paths vendor/drupal/coder/coder_sniffer/
+vendor/bin/phpcs --config-set default_standard Drupal,DrupalPractice
+
+# Install Drupal
 vendor/bin/drush -y site:install \
   standard \
   --site-name="gitpod-drupal" \
